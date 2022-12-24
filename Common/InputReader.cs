b354036f21917c350f844c9545bc80e53d05537c
Javier Astudillo <javier_astudillo@live.com.mx>
2022-12-24 15:05:44 -0600

    Template

---
+++ Common/InputReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Common
+{
+    internal class InputReader
+    {
+        public static async Task<string[]>  GetInput(string path)
+        {
+            var text = await File.ReadAllLinesAsync(path);
+
+            return text;
+        }
+    }
+}
---
+++ Days/BaseDay.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2022.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Days
+{
+    internal abstract class BaseDay
+    {
+        public string DayName { get; init; }
+        public string[] InputLines { get; init; }
+
+        public BaseDay(string dayName)
+        {
+            DayName = dayName;
+
+            var path = $"Inputs\\{dayName}";
+            InputLines = InputReader.GetInput(path).GetAwaiter().GetResult();
+        }
+    }
+}
