1b816adbe61058c1f83c34a53ff3bedc6757a0ad
Javier Astudillo <astudisho@gmail.com>
2023-11-08 02:56:20 -0600

    Incomplete day 11

--- Program.cs
+++ Program.cs
@@ -5,26 +5,31 @@ Console.WriteLine("Hello, World!");
 
 // var day3 = new Day3();
 
-//var day3Result1 = day3.Solve1();
-//var day3Result2 = day3.Solve2();
+// var day3Result1 = day3.Solve1();
+// var day3Result2 = day3.Solve2();
 
-//var day4 = new Day4();
+// var day4 = new Day4();
 
-//var day4Result1 = day4.Solve1();
-//var day4Result2 = day4.Solve2();
+// var day4Result1 = day4.Solve1();
+// var day4Result2 = day4.Solve2();
 
-//var day5 = new Day5();
+// var day5 = new Day5();
 
-//var day5Result1 = day5.Solve1();
-//var day5Result2 = day5.Solve2();
+// var day5Result1 = day5.Solve1();
+// var day5Result2 = day5.Solve2();
 
-//var day6 = new Day6();
-//var day6Result1 = day6.Solve1();
-//var day6Result2 = day6.Solve2();
+// var day6 = new Day6();
+// var day6Result1 = day6.Solve1();
+// var day6Result2 = day6.Solve2();
 
-var day10 = new Day10();
+// var day10 = new Day10();
 
-//day10.Solve1();
-day10.Solve2();
+// day10.Solve1();
+// day10.Solve2();
+
+var day11 = new Day11();
+
+// day11.Solve1();
+day11.Solve2();
 
 Console.ReadLine();
