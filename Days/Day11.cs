ae399216b80812df6fe3135b5a301d6759cbaba6
Javier Astudillo <astudisho@gmail.com>
2023-11-08 02:56:34 -0600

    Incomplete day 11

---
+++ Days/Day11.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using AdventOfCode2022.Days.Helpers;
+
+namespace AdventOfCode2022.Days
+{
+	public class Day11 : BaseDay
+	{
+        public Day11() : base(nameof(Day11), "Inputs")
+        {
+        }
+
+        public override string Solve1()
+        {
+            var monkeys = Day11Helper.GetMonkeys();
+            var maxRounds = 20;
+
+            for (int i = 0; i < maxRounds; i++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    solve1Action(monkey, monkeys);
+                }
+            }
+
+            var mostActiveMonkeys = monkeys
+                .OrderByDescending(x => x.TimesInspected)
+                .Take(2)
+                .ToArray();
+            var result = mostActiveMonkeys[0].TimesInspected * mostActiveMonkeys[1].TimesInspected;
+            return result.ToString();
+        }
+
+        Action<Monkey, Monkey[]> solve1Action = (Monkey monkey, Monkey[] monkeys) =>
+        {
+            while(monkey.ItemsQueue.Any())
+            {
+                var item = monkey.ItemsQueue.Dequeue();
+
+                var newItem = monkey.Operation(item);
+
+                newItem /= 3;
+
+                var testResult = monkey.Test(newItem);
+
+                if (testResult)
+                {
+                    monkeys[monkey.ThrowTo.ifTrue].ItemsQueue.Enqueue(newItem);
+                }
+                else
+                {
+                    monkeys[monkey.ThrowTo.ifFalse].ItemsQueue.Enqueue(newItem);
+                }
+
+                monkey.TimesInspected++;
+            }
+        };
+
+        Action<Monkey, Monkey[]> solve2Action = (Monkey monkey, Monkey[] monkeys) =>
+        {
+            while (monkey.ItemsQueue.Any())
+            {
+                var item = monkey.ItemsQueue.Dequeue();
+
+                var newItem = monkey.Operation(item);
+
+                newItem %= 96577;
+
+                var testResult = monkey.Test(newItem);
+
+                if (testResult)
+                {
+                    monkeys[monkey.ThrowTo.ifTrue].ItemsQueue.Enqueue(newItem);
+                }
+                else
+                {
+                    monkeys[monkey.ThrowTo.ifFalse].ItemsQueue.Enqueue(newItem);
+                }
+
+                monkey.TimesInspected++;
+            }
+        };
+
+        public override string Solve2()
+        {
+            var monkeys = Day11Helper.GetMonkeysTest();
+            var maxRounds = 1000;
+
+            for (int i = 0; i < maxRounds; i++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    solve2Action(monkey, monkeys);
+                }
+            }
+
+            var mostActiveMonkeys = monkeys
+                .OrderByDescending(x => x.TimesInspected)
+                .Take(2)
+                .ToArray();
+            long result = mostActiveMonkeys[0].TimesInspected * mostActiveMonkeys[1].TimesInspected;
+            return result.ToString();
+        }
+    }
+}
+
---
+++ Days/Helpers/Day11Helper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+namespace AdventOfCode2022.Days.Helpers
+{
+	public static class Day11Helper
+	{
+		public static Monkey[] GetMonkeysTest() => new Monkey[]
+		{
+			new Monkey
+			{
+				ItemsQueue = new(new List<int> { 79, 98 } ),
+				Operation = (old) => old * 19,
+				Test = (n) => n % 23 == 0,
+				ThrowTo = (2,3),
+			},
+			new()
+			{
+				ItemsQueue = new (new List<int>(){ 54, 65, 75, 74 }),
+				Operation = (old) => old + 6,
+				Test = (n) => n % 19 == 0,
+				ThrowTo = (2,0),
+			},
+			new()
+			{
+				ItemsQueue = new (new List<int> { 79, 60, 97 }),
+				Operation = (old) => old * old,
+				Test = (n) => n % 13 == 0,
+				ThrowTo = (1 ,3)
+			},
+			new()
+			{
+				ItemsQueue = new(new List<int> { 74 }),
+				Operation = (old) => old + 3,
+				Test = (n) => n % 17 is 0,
+				ThrowTo = (0 , 1)
+			}
+		};
+
+        public static Monkey[] GetMonkeys() => new Monkey[]
+        {
+            new Monkey
+            {
+                ItemsQueue = new(new List<int> { 74, 64, 74, 63, 53 } ),
+                Operation = (old) => old * 7,
+                Test = (n) => n % 5 == 0,
+                ThrowTo = (1,6),
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int>(){ 69, 99, 95, 62 }),
+                Operation = (old) => old * old,
+                Test = (n) => n % 17 == 0,
+                ThrowTo = (2,5),
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int> { 59, 81 }),
+                Operation = (old) => old + 8,
+                Test = (n) => n % 7 == 0,
+                ThrowTo = (4 ,3)
+            },
+            new()
+            {
+                ItemsQueue = new(new List<int> { 50, 67, 63, 57, 63, 83, 97 }),
+                Operation = (old) => old + 4,
+                Test = (n) => n % 13 == 0,
+                ThrowTo = (0 , 7)
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int> { 61, 94, 85, 52, 81, 90, 94, 70 }),
+                Operation = (old) => old + 3,
+                Test = (n) => n % 19 == 0,
+                ThrowTo = (7 ,3)
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int> { 69 }),
+                Operation = (old) => old + 5,
+                Test = (n) => n % 3 == 0,
+                ThrowTo = (4 ,2)
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int> { 54, 55 ,58 }),
+                Operation = (old) => old + 7,
+                Test = (n) => n % 11 == 0,
+                ThrowTo = (1 , 5)
+            },
+            new()
+            {
+                ItemsQueue = new (new List<int> { 79, 51, 83, 88, 93, 76 }),
+                Operation = (old) => old * 3,
+                Test = (n) => n % 2 == 0,
+                ThrowTo = (0 ,6)
+            },
+        };
+    }
+
+	public class Monkey
+	{
+		public Queue<int> ItemsQueue { get; set; } = new();
+
+		public Func<int, int>? Operation;
+
+		public Func<int, bool>? Test;
+
+		public (int ifTrue, int ifFalse) ThrowTo;
+
+		public long TimesInspected { get; set; } = 0;
+	}
+}
